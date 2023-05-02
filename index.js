36521579a03fd30eed0ecbefc4449a1786a2128a
misna04 <misnasyari@gmail.com>
2023-05-02 14:40:38 +0800

    config nodejs

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+// Serve the public directory as a static directory
+app.use(express.static(path.join(__dirname, "public")))
+
+// Handle other routes or middleware as needed
+// ...
+
+// Start the server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
